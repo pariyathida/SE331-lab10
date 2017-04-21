cdde264d00f6ace287750e3861e07f99a32b7edc
DESKTOP-9CKSS1Q\CAMT <pariyathida@gmail.com>
2017-04-21 16:11:23 +0700

    2.8

--- src/main/java/camt/cbsd/config/DataLoader.java
+++ src/main/java/camt/cbsd/config/DataLoader.java
@@ -4,16 +4,26 @@ import camt.cbsd.dao.CourseDao;
 import camt.cbsd.dao.StudentDao;
 import camt.cbsd.entity.Course;
 import camt.cbsd.entity.Student;
+import camt.cbsd.entity.security.Authority;
+import camt.cbsd.entity.security.AuthorityName;
+import camt.cbsd.entity.security.User;
 import camt.cbsd.repository.StudentRepository;
+import camt.cbsd.security.repository.AuthorityRepository;
+import camt.cbsd.security.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import sun.net.www.protocol.http.AuthenticationInfo;
 
 import javax.annotation.PostConstruct;
 import javax.transaction.Transactional;
 import java.beans.Transient;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * Created by Dto on 07-Apr-17.
@@ -22,6 +32,13 @@ import java.beans.Transient;
 @Component
 public class DataLoader implements ApplicationRunner{
     StudentDao studentDao;
+
+    @Autowired
+    UserRepository userSecurityRepository;
+
+    @Autowired
+    AuthorityRepository authorityRepository;
+
     @Autowired
     public void setStudentDao(StudentDao studentDao) {
         this.studentDao = studentDao;
@@ -48,6 +65,7 @@ public class DataLoader implements ApplicationRunner{
     @Override
     @Transactional
     public void run(ApplicationArguments args) throws Exception {
+
         imageBaseUrl = baseUrl + imageUrl;
         Student student1 = Student.builder().studentId("SE-001").name("Mitsuha").surname("Miyamizu")
                 .gpa(2.15).image(imageBaseUrl+"mitsuha.gif").feature(true)
@@ -77,6 +95,57 @@ public class DataLoader implements ApplicationRunner{
         student3.addCourse(course1);
         student3.addCourse(course3);
 
+        securitySetup();
+    }
+
+    public void securitySetup(){
+        User user1 = User.builder()
+                .username("admin")
+                .password("admin")
+                .firstname("admin")
+                .lastname("admin")
+                .email("admin@admin.com")
+                .enabled(true)
+                .lastPasswordResetDate(Date.from(LocalDate.of(2016,01,01).atStartOfDay(ZoneId.systemDefault()).toInstant()))
+                .build();
+
+        User user2 = User.builder()
+                .username("user")
+                .password("user")
+                .firstname("user")
+                .lastname("user")
+                .email("enabled@user.com")
+                .enabled(true)
+                .lastPasswordResetDate(Date.from(LocalDate.of(2016,01,01).atStartOfDay(ZoneId.systemDefault()).toInstant()))
+                .build();
+        User user3 = User.builder()
+                .username("disabled")
+                .password("disabled")
+                .firstname("user")
+                .lastname("user")
+                .email("disabled@user.com")
+                .enabled(false)
+                .lastPasswordResetDate(Date.from(LocalDate.of(2016,01,01).atStartOfDay(ZoneId.systemDefault()).toInstant()))
+                .build();
+        Authority auth1 = Authority.builder().name(AuthorityName.ROLE_USER).build();
+        Authority auth2 = Authority.builder().name(AuthorityName.ROLE_ADMIN).build();
+            authorityRepository.save(auth1);
+            authorityRepository.save(auth2);
+            user1.setAuthorities(new ArrayList<>());
+            user1.getAuthorities().add(auth1);
+            user1.getAuthorities().add(auth2);
+            user2.setAuthorities(new ArrayList<>());
+            user2.getAuthorities().add(auth1);
+            user3.setAuthorities(new ArrayList<>());
+            user3.getAuthorities().add(auth1);
+
+            userSecurityRepository.save(user1);
+            userSecurityRepository.save(user2);
+            userSecurityRepository.save(user3);
 
     }
+
+
+
+
 }
